3e993589ad478ae90233dded09b253bac4db0f19
Marion Zumbado <mzumbado@cecropiasolutions.com>
2013-08-28 15:59:08 -0600

    inicio de editar carrito de compras

--- CMSKreaideas/app/models/cart.rb
+++ CMSKreaideas/app/models/cart.rb
@@ -1,6 +1,7 @@
 class Cart < ActiveRecord::Base
   # attr_accessible :title, :body
 	has_many :line_items, :dependent => :destroy
+	accepts_nested_attributes_for :line_items, :allow_destroy => true
 
 	def add_product(product_id)
 		current_item = line_items.where(:product_id => product_id).first
--- CMSKreaideas/app/models/line_item.rb
+++ CMSKreaideas/app/models/line_item.rb
@@ -2,4 +2,14 @@ class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id,:product
   belongs_to :product ,:class_name => '::Refinery::Products::Product'
   belongs_to :cart
+  accepts_nested_attributes_for :product
+
+  	HUMANIZED_ATTRIBUTES = {
+    :quantity => "Cantidad"
+ 	 }
+
+    def self.human_attribute_name(attr, options={})
+    	HUMANIZED_ATTRIBUTES[attr.to_sym] || super
+  	end
+
 end
