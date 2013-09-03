a3f2e039d2b2b1605d2bbad29b798fa1199553f9
Marion Zumbado <mzumbado@cecropiasolutions.com>
2013-09-03 09:39:49 -0600

    al cerrar el explorador la sesion x la cookie se guarda por 1 dia

--- CMSKreaideas/config/initializers/session_store.rb
+++ CMSKreaideas/config/initializers/session_store.rb
@@ -1,6 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
-CMSKreaideas::Application.config.session_store :cookie_store, key: '_CMSKreaideas_session'
+#CMSKreaideas::Application.config.session_store :cookie_store, key: '_CMSKreaideas_session'
+
+CMSKreaideas::Application.config.session_store :cookie_store, {
+  expire_after: 24.hours,
+}
+
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
